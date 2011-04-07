54db34be5cade4bc8f00aa4502dc64c05ff0ac7c
Jan Max Meyer <none@none>
2011-04-07 02:57:38 +0200

    SCCS p_util.c 11/01/24 00:11:16: Removed old code, added new function p_gen_c_identifier()

--- p_util.c
+++ p_util.c
@@ -1,6 +1,6 @@
 /* -MODULE----------------------------------------------------------------------
 UniCC LALR(1) Parser Generator 
-Copyright (C) 2006-2009 by Phorward Software Technologies, Jan Max Meyer
+Copyright (C) 2006-2011 by Phorward Software Technologies, Jan Max Meyer
 http://unicc.phorward-software.com/ ++ unicc<<AT>>phorward-software<<DOT>>com
 
 File:	p_util.c
@@ -207,236 +207,6 @@ int p_unescape_char( uchar* str, uchar** strfix )
 	return ch;
 }
 
-
-/* -FUNCTION--------------------------------------------------------------------
-	Function:		p_ccl_to_map()
-	
-	Author:			Jan Max Meyer
-	
-	Usage:			Parses a character class definition and maps it in a bitset
-					with parser->p_universe size, where enabled characters are
-					toggled TRUE.
-
-	Parameters:		PARSER*		parser		Parser information structure
-					uchar*		ccl			Character class to be parsed/converted
-					
-	Returns:		bitset					Bitmap representing the character
-											states. This is automatically allocated,
-											must be freed by caller.
-  
-	~~~ CHANGES & NOTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	Date:		Author:			Note:
------------------------------------------------------------------------------ */
-bitset p_ccl_to_map( PARSER* parser, uchar* ccl )
-{
-	uchar*		ptr;
-	bitset		map;
-	BOOLEAN		neg		= FALSE;
-	int			start;
-	int			stop;
-	int			i;
-
-	if( !parser || !ccl )
-		return (bitset)NULL;
-
-	map = bitset_create( parser->p_universe );
-	if( !map )
-		return (bitset)NULL;
-
-	ptr = ccl;
-
-	/*
-	if( *ptr == '^' )
-	{
-		neg = TRUE;
-		for( i = 0; i < parser->p_universe; i++ )
-			bitset_set( map, i, 1 );
-	}
-	*/
-
-	for( ; *ptr; )
-	{
-		start = p_unescape_char( ptr, &ptr );
-		if( *(ptr) == '-' )
-		{
-			ptr++;
-			stop = p_unescape_char( ptr, &ptr );
-			
-			for( i = ( start > stop ? stop : start );
-					i <= ( start > stop ? start : stop );
-						i++ )
-				bitset_set( map, i, neg ? 0 : 1 );
-		}
-		else
-			bitset_set( map, start, neg ? 0 : 1 );
-	}
-
-	return map;
-}
-
-/* -FUNCTION--------------------------------------------------------------------
-	Function:		p_map_to_ccl()
-	
-	Author:			Jan Max Meyer
-	
-	Usage:			Parsers a character class map and maps it into a character
-					class definition string.
-
-	Parameters:		PARSER*		parser		Parser information structure
-					bitset		map			Character class map to be converted
-					
-	Returns:		uchar*					The character-class definition string.
-											This is automatically allocated, must
-											be freed by caller.
-  
-	~~~ CHANGES & NOTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	Date:		Author:			Note:
------------------------------------------------------------------------------ */
-uchar* p_map_to_ccl( PARSER* parser, bitset map )
-{
-#define DEF_FOR_RANGE( fromchar, tochar ) \
-	{ \
-		if( (fromchar) >= 32 && (fromchar) <= 126 && (fromchar) != '\\' ) \
-			sprintf( tmp, "%c", (uchar)(fromchar) ); \
-		else \
-			sprintf( tmp, "\\%d", (fromchar) ); \
-		\
-		sprintf( tmp + strlen( tmp ), "-" ); \
-		\
-		if( (tochar) >= 32 && (tochar) <= 126 && (tochar) != '\\' ) \
-			sprintf( tmp + strlen( tmp ), "%c", (uchar)(tochar) ); \
-		else \
-			sprintf( tmp + strlen( tmp ), "\\%d", (tochar) ); \
-	}
-
-#define DEF_FOR_CHAR( chr ) \
-	{ \
-		if( (chr) >= 32 && (chr) <= 126 && (chr) != '\\' ) \
-			sprintf( tmp, "%c", (uchar)(chr) ); \
-		else \
-			sprintf( tmp, "\\%d", (chr) ); \
-	}
-
-
-	uchar*		ret		= (uchar*)NULL;
-	uchar		tmp		[ 30 + 1 ];
-	int			i, begin = -1;
-
-	for( i = 0; i < parser->p_universe; i++ )
-	{
-		if( begin == -1 && bitset_get( map, i ) )
-			begin = i;
-		else if( !( bitset_get( map, i ) ) && begin > -1 )
-		{
-			/* Is this a range? */
-			if( begin == i - 1 )
-				DEF_FOR_CHAR( begin )
-			else				
-				DEF_FOR_RANGE( begin, i - 1 )
-
-			ret = p_str_append( ret, tmp, FALSE );
-
-			if( !ret )
-				return (uchar*)NULL;
-
-			begin = -1;
-		}
-	}
-
-	if( begin > -1 )
-	{
-		if( begin == i - 1 )
-			DEF_FOR_CHAR( begin )
-		else				
-			DEF_FOR_RANGE( begin, i - 1 )
-
-		ret = p_str_append( ret, tmp, FALSE );
-	}
-
-	return ret;
-
-#undef DEF_FOR_RANGE
-#undef DEF_FOR_CHAR
-}
-
-/* -FUNCTION--------------------------------------------------------------------
-	Function:		p_negate_ccl()
-	
-	Author:			Jan Max Meyer
-	
-	Usage:			Negates a character class definition and constructs a new
-					definition string.
-
-	Parameters:		PARSER*		parser		Parser information structure
-					uchar*		ccl			Character class to be negated.
-					
-	Returns:		uchar*					The character-class definition string.
-											This is automatically allocated, must
-											be freed by caller.
-  
-	~~~ CHANGES & NOTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	Date:		Author:			Note:
------------------------------------------------------------------------------ */
-uchar* p_negate_ccl( PARSER* parser, uchar* ccl )
-{
-	bitset	tmp;
-	int		i;
-
-	if( !( tmp = p_ccl_to_map( parser, ccl ) ) )
-		OUT_OF_MEMORY;
-
-	for( i = 0; i < parser->p_universe; i++ )
-	{
-		if( bitset_get( tmp, i ) )
-			bitset_set( tmp, i, 0 );
-		else
-			bitset_set( tmp, i, 1 );
-	}
-
-	ccl = p_map_to_ccl( parser, tmp );
-	p_free( tmp );
-
-	return ccl;
-}
-
-/* -FUNCTION--------------------------------------------------------------------
-	Function:		p_map_test_char()
-	
-	Author:			Jan Max Meyer
-	
-	Usage:			Performs a bitset_get(), but with case-insensitivity, if
-					desired.
-
-	Parameters:		bitset		map			Pointer to map to test on
-					uchar		chr			Character to be tested
-					BOOLEAN		insensitive	TRUE: Test case-insensitive
-											FALSE: Test case-sensitive
-					
-	Returns:		BOOLEAN		TRUE		Bit is set
-								FALSE		Bit is unset
-  
-	~~~ CHANGES & NOTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	Date:		Author:			Note:
------------------------------------------------------------------------------ */
-BOOLEAN p_map_test_char( bitset map, uchar chr, BOOLEAN insensitive )
-{
-	if( bitset_get( map, (int)chr ) )
-		return TRUE;
-		
-	if( insensitive )
-	{
-		if( chr >= 'A' && chr <= 'Z' )
-			chr += 32;
-		else if( chr >= 'a' && chr <= 'z' )
-			chr -= 32;
-			
-		if( bitset_get( map, (int)chr ) )
-			return TRUE;
-	}
-	
-	return FALSE;
-}
-
 /* -FUNCTION--------------------------------------------------------------------
 	Function:		p_ccl_test_char()
 	
@@ -487,3 +257,39 @@ SYMBOL* p_find_base_symbol( SYMBOL* sym )
 
 	return sym;
 }
+
+/* -FUNCTION--------------------------------------------------------------------
+	Function:		p_gen_c_identifier()
+	
+	Author:			Jan Max Meyer
+	
+	Usage:			Construct a C-identifier from a file-name.
+					
+	Parameters:		<type>		<identifier>		<description>
+	
+	Returns:		<type>							<description>
+  
+	~~~ CHANGES & NOTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Date:		Author:			Note:
+----------------------------------------------------------------------------- */
+uchar* p_gen_c_identifier( uchar* str, BOOLEAN to_upper )
+{
+	uchar*	p;
+
+	if( !( str = pstrdup( str ) ) )
+	 	OUT_OF_MEMORY;
+
+	for( p = str; *p; p++ )
+	{
+		if( pisalnum( *p ) )
+		{
+			if( to_upper )
+				*p = ptoupper( *p );
+		}
+		else
+			*p = '_';
+	}
+
+	return str;
+}
+
