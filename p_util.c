0088cbf85561b3c6d70f477adfd4430588d56cbc
Jan Max Meyer <none@none>
2011-04-07 02:57:36 +0200

    SCCS p_util.c 09/04/18 13:41:52: Added Artistic License 2 clause and updated copyright in header comments

--- p_util.c
+++ p_util.c
@@ -1,11 +1,14 @@
 /* -MODULE----------------------------------------------------------------------
 UniCC LALR(1) Parser Generator 
- (C) 2006-2009 by Phorward Software Technologies, Jan Max Meyer
-http://www.phorward-software.com ++ mail@phorward-software.com
+Copyright (C) 2006-2009 by Phorward Software Technologies, Jan Max Meyer
+http://unicc.phorward-software.com/ ++ unicc<<AT>>phorward-software<<DOT>>com
 
 File:	p_util.c
 Author:	Jan Max Meyer
 Usage:	Utility functions
+
+You may use, modify and distribute this software under the terms and conditions
+of the Artistic License, version 2. Please see LICENSE for more information.
 ----------------------------------------------------------------------------- */
 
 /*
