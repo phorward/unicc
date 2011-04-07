3c7808ebd80e47e2c76e2bb893c467c72c6607cb
Jan Max Meyer <none@none>
2011-04-07 02:57:27 +0200

    SCCS p_proto.h 09/04/29 12:34:54: New command-line switch '--symbols'

--- p_proto.h
+++ p_proto.h
@@ -91,6 +91,7 @@ SYMBOL* p_optional_closure( PARSER* parser, SYMBOL* base );
 /* p_debug.c */
 void p_print_symbol( FILE* stream, SYMBOL* sym );
 void p_dump_grammar( FILE* stream, PARSER* parser );
+void p_dump_symbols( FILE* stream, PARSER* parser );
 void p_dump_item_set( FILE* stream, uchar* title, LIST* list );
 void p_dump_map( FILE* stream, bitset map, int map_size );
 void p_dump_lalr_states( FILE* stream, PARSER* parser );
