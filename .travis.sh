af7821712952d89cb925d943c0f573b239f1c9c1
Jan Max Meyer <jmm@phorward.de>
2019-01-05 00:15:34 +0100

    replaced pushd/popd by cd

--- .travis.sh
+++ .travis.sh
@@ -6,11 +6,11 @@ LIBPHORWARD_VERSION="0.24.0"
 # Phorward C/C++ Library
 wget https://github.com/phorward/phorward/archive/v${LIBPHORWARD_VERSION}.tar.gz
 tar xvfz v${LIBPHORWARD_VERSION}.tar.gz
-pushd phorward-${LIBPHORWARD_VERSION}
+cd phorward-${LIBPHORWARD_VERSION}
 ./configure --prefix=/usr
 make
 sudo make install
-popd
+cd ..
 
 # UniCC
 touch aclocal.m4 configure Makefile.am Makefile.in
