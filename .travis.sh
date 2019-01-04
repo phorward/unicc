ef2114563864b1999862325bcd1e843fa03bde9b
Jan Max Meyer <jmm@phorward.de>
2019-01-05 00:12:42 +0100

    Changes to Travis CI build scripts

--- .travis.sh
+++ .travis.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 set -ex
+
+LIBPHORWARD_VERSION="0.24.0"
+
+# Phorward C/C++ Library
+wget https://github.com/phorward/phorward/archive/v${LIBPHORWARD_VERSION}.tar.gz
+tar xvfz v${LIBPHORWARD_VERSION}.tar.gz
+pushd phorward-${LIBPHORWARD_VERSION}
+./configure --prefix=/usr
+make
+sudo make install
+popd
+
+# UniCC
+touch aclocal.m4 configure Makefile.am Makefile.in
 ./configure
 make
 sudo make install
