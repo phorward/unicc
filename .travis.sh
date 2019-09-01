0fa931d50760c90602a8d4c056af6c96e57c7c2b
Jan Max Meyer <jmm@phorward.de>
2019-09-01 13:38:50 +0200

    Removing libphorward fetch before

--- .travis.sh
+++ .travis.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 set -ex
 
-LIBPHORWARD_VERSION="0.24.0"
-
-# Phorward C/C++ Library
-wget https://github.com/phorward/phorward/archive/v${LIBPHORWARD_VERSION}.tar.gz
-tar xvfz v${LIBPHORWARD_VERSION}.tar.gz
-cd phorward-${LIBPHORWARD_VERSION}
-./configure --prefix=/usr
-make
-sudo make install
-cd ..
-
-# UniCC
 touch aclocal.m4 configure Makefile.am Makefile.in
 ./configure
 make
 sudo make install
+
 unicc -V
+
 make -f Makefile.gnu test
